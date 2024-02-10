39a5c98e059f5148e595698e189542aebf4808a4
Rahman <abdulrahman6093@outlook.com>
2024-02-10 20:24:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Carousel } from 'react-carousel3';
+import Image1 from './assests/image_1.jpg';
+import Image2 from './assests/image_2.jpg';
+import Image3 from './assests/image_3.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel className="carousel" height={300} width={500} yOrigin={42} yRadius={48} autoPlay={true}>
+        <div key={1}>
+          <img alt="one" src={Image1}/>
+        </div>
+        <div key={2}>
+          <img alt="two" src={Image2}/>
+        </div>
+        <div key={3}>
+          <img alt="three" src={Image3}/>
+        </div>
+      </Carousel>
     </div>
   );
 }
